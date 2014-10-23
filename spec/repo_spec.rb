6409e4eb9cc75eb8ab2b0700b98690a4087546bb
Chris Gianelloni <chris@cask.co>
2014-10-22 17:16:21 -0700

    Use ChefSpec::SoloRunner instead of ChefSpec::Runner

--- spec/repo_spec.rb
+++ spec/repo_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
-describe 'coopr::default' do
+describe 'coopr::repo' do
   context 'on Centos 6.5 x86_64' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
         node.automatic['domain'] = 'example.com'
-        stub_command('update-alternatives --display coopr-conf | grep best | awk \'{print $5}\' | grep /etc/cdap/conf.chef').and_return(false)
       end.converge(described_recipe)
     end
 
