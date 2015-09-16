b8094e9b6c58f3cbf6f39d7e3db392a52e8e365f
Chris Gianelloni <chris@cask.co>
2015-09-16 15:45:20 -0700

    Update license in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'coopr'
 maintainer       'Cask'
 maintainer_email 'ops@cask.co'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures Coopr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
