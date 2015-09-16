6ca0a78a3b830f0cd915280c887f3bd35d02f0c5
Chris Gianelloni <chris@cask.co>
2015-09-16 15:41:35 -0700

    Version bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'All rights reserved'
 description      'Installs/Configures Coopr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 %w(apt java yum).each do |cb|
   depends cb
